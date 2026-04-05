b730efdbec8a889de66105ccb06517c6eaac7563
anikhere <tahaanik729@gmail.com>
2026-04-05 10:30:56 +0530

    2nd error solving

--- src/summarizer_model/components/trainer.py
+++ src/summarizer_model/components/trainer.py
@@ -12,6 +12,8 @@ class ModelTrainer:
     def __init__(self, config:model_trainer, trans_art:DataTransArtifact):
         self.config = config
         self.dt = trans_art
+        self.params = Load_yaml(PARAMS_YAML).TrainingArgument
+
         logger.info(f'starting the training phase now ')
     def train(self):
         train_data = load_from_disk(self.dt.transformed_train_path)
@@ -21,10 +23,12 @@ class ModelTrainer:
         model_pegassus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_cpkt).to(device)
         seq_to_seq = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model_pegassus)
         trainer_args = TrainingArguments(
-            output_dir = self.params, num_train_epochs=1,warmup_steps=500,
-            per_device_train_batch_size = 1,per_device_eval_batch_size = 1,
-            weight_decay = 0.01,logging_steps=10,evaluation_strategy = 'steps',
-            eval_steps = 500,save_steps=1e6,gradient_accumulation_steps = 16
+            output_dir =str(self.config.root_dir)
+            ,num_train_epochs=self.params.num_train_epochs,warmup_steps=self.params.warmup_steps,
+            per_device_train_batch_size = self.params.per_device_train_batch_size,per_device_eval_batch_size = self.params.per_device_eval_batch_size,
+            weight_decay = self.params.weight_decay,logging_steps=self.params.logging_steps,evaluation_strategy = self.params.evaluation_strategy
+,
+            eval_steps = self.params.eval_steps,save_steps=self.params.save_steps,gradient_accumulation_steps = self.params.gradient_accumulation_steps
         )
         trainer = Trainer(model = model_pegassus,args=trainer_args,
                           tokenizer = tokenizer,data_collator = seq_to_seq,
